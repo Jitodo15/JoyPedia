dff80b68e528cc29e8fc54604220d9de26b34d8e
Jitodo15 <joyoneh.15@gmail.com>
2023-09-01 18:49:36 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# JoyPedia
+
+JoyPedia is a simple search tool that utilizes the Wikipedia API to fetch and display search results from Wikipedia articles. It provides the users with the ability to search for topics and get summaries of their search.
+
+## Demo
+<img height=300, src="JoyPedia.gif">
+
+### Features
+1. Search for topics
+2. Display first three summaries related to the search query
+3. Error handling for failed API requests or no search results.
+4. Built with Python and the Tkinter library for GUI.
+
+### Installation
+1. CLone or download the repository to your device.
+2. Make sure you have Python 3.6 or higher installed
+3. Install the "requests" package.
+
+### Usage
+1. Run the "main.py" file to launch the app.
+2. Enter your search query in the input field.
+3. Click the "Search" button to fetch and display Wikipedia search results.
+4. Top three summaries of the related search will be displayed in the output area.
+5. In case of spelling errors, an error message will be shown.
+
+### Acknowledgements
+1. This project uses Wikipedia API gotten from the RapidAPI website to retrieve data.
+2. This project uses the 'requests' library provided by the authors.
---
+++ main.py
@@ -0,0 +1,70 @@
+from tkinter import *
+import requests
+
+API_KEY = 'c48212d7a6msh0749ca07062c951p1d91dcjsn457de6d0ed23'
+API_HOST = 'wiki-briefs.p.rapidapi.com'
+
+
+def get_query():
+
+    query = entry.get()
+
+    params = {
+        "q": query,
+        "topk": 3
+    }
+
+    headers = {
+        'X-RapidAPI-Key': API_KEY,
+        'X-RapidAPI-Host': API_HOST
+      }
+
+    try:
+        response = requests.get(url='https://wiki-briefs.p.rapidapi.com/search', params=params, headers=headers)
+        response.raise_for_status()
+        data = response.json()["summary"]
+
+        if not data:
+            canvas.delete("some_tag")
+            canvas.create_text(350, 250, width=550, text="Search not found. Check for spelling errors.",
+                               font=("Arial", 40, "bold"), tags="some_tag")
+            canvas.grid(row=3, column=0, columnspan=5)
+
+        else:
+            summaries = []
+            for item in data:
+                summaries.append(item)
+            canvas.delete("some_tag")
+            canvas.create_text(350, 250, width=550, text="\n\n".join(summaries), font=("Arial", 20), tags="some_tag")
+            canvas.grid(row=3, column=0, columnspan=5)
+
+    except requests.exceptions.RequestException as e:
+        canvas.delete("some_tag")
+        canvas.create_text(350, 250, text="Search not found. Check for spelling errors.", font=("Arial", 20, "bold"), anchor="center", tags="some_tag")
+
+    entry.delete(0, END)
+
+
+window = Tk()
+window.title("JoyPedia")
+window.minsize(width=600, height=600)
+window.configure(pady=50, padx=50, bg='darkseagreen1')
+
+
+heading = Label(text="JoyPedia", font=('Verdana', 36, 'bold'), bg='darkseagreen1')
+message = Label(text='Enter search', font=('Verdana', 16), bg='darkseagreen1')
+entry = Entry(font=('Verdana', 16), width=40)
+canvas = Canvas(width=700, height=500, bg="white")
+# output = Label(font=('Verdana', 16), bg='darkseagreen1', wraplength=400, justify='left')
+
+
+search_button = Button(text='Search', font=('Verdana', 16), command=get_query)
+
+heading.grid(row=0, column=1,  columnspan=3, pady=10)
+message.grid(row=2, column=0)
+
+entry.grid(row=2, column=1, columnspan=3, padx=10, pady=10)
+search_button.grid(row=2, column=4)
+
+
+mainloop()
